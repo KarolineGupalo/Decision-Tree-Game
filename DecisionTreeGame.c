0d6f52bef2ba1afbd0d6ec30b9f327c71472ac41
KarolineGupalo <112424723+KarolineGupalo@users.noreply.github.com>
2023-07-19 14:10:03 +0200

    Update DecisionTreeGame.c

--- DecisionTreeGame.c
+++ DecisionTreeGame.c
@@ -5,8 +5,21 @@
 
 DecisionTreeNode* createNode(const char* question)
 {
-  DecisionTreeNode* newNode = (DecisionTreeNode*)malloc(sizeof(DecisionTreeNode));
-
+  DecisionTreeNode* newNode = (DecisionTreeNode*)calloc(1, sizeof(DecisionTreeNode));
   if(newNode)
   {
-    newNode->question
+      newNode->question = (char*)malloc(strlen(question) + 1);
+      if(newNode->question)
+      {
+          strcpy(newNode->question, question)
+          newNode->yes = NULL;
+          newNode->no = NULL;
+      }
+      else
+      {
+          free(newNode);
+          newNode = NULL;
+      }
+  } 
+  return newNode;
+}
