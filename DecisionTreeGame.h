eae8bbdfe52fcf97d620ad51e563ce317c3718fc
KarolineGupalo <112424723+KarolineGupalo@users.noreply.github.com>
2023-08-29 22:43:08 +0200

    Update DecisionTreeGame.h
    Kommentare verbessert, um die Funktionen besser und einfacher zu verstehen.

--- DecisionTreeGame.h
+++ DecisionTreeGame.h
@@ -5,10 +5,9 @@ typedef struct DecisionTreeNode
     struct DecisionTreeNode* no; //Pointer auf die "Nein"-Entscheidung
 } DecisionTreeNode;
 
-DecisionTreeNode* createNode(const char* question); //Diese Funktion erzeugt einen neuen BST
+DecisionTreeNode* createNode(const char* question); //Diese Funktion erzeugt einen neuen Knoten im BST und damit auch den BST selbst. Nimmt String und gibt einen Pointer auf die Frage. 
 
-void insertQuestion(DecisionTreeNode* root, const char* question, char answer); //Fügt einen Knoten mit einer Frage hinzu. Die Antwortparameter beinhalten 'y' für yes und 'n' für no.
-
-void playGame(DecisionTreeNode* root); //Traversiert den Baum und lässt das Spielen zu.
+void insertQuestion(DecisionTreeNode* root, const char* question, char answer); //Hier werde neue Nodes mit dem bereits bestehenden Node mittels einem Pointer für 'yes' oder 'no' verbunden.
+void playGame(DecisionTreeNode* root); //Spieler kann durch den BST navigieren und beginnt beim root. Die Funktion wird, basierend auf der Auswahl der spielenden Person, traversiert, was zu unterschiedlichen Enden des Spiels führt.
 
 void freeTree(DecisionTreeNode* root); //Befreit den für das Spiel reservierten Speicher.
